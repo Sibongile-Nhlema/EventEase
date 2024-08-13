a913b9fbf7ee02f604451806f43c93ae3c0b8867
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-08-13 14:50:14 +0200

    Setting Flask But Not yet completed
    installed Flask,
    Setted up the enviroment and activated it
    Made it to run as one with python app.py

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request, redirect, send_from_directory, url_for, session
+from flask_cors import CORS
+import os
+
+app = Flask(__name__, static_folder='../frontend/build')
+CORS(app)
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {"message": "Hello from Flask!"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
