749011a7fe10c06e929a18270edba9110261ab6c
Sibongile <sibongile.nhlema@gmail.com>
2024-08-15 18:23:59 +0200

    connected react to flask

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
+CORS(app)
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    return send_from_directory(app.static_folder, path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
